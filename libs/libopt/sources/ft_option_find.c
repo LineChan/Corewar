59d8ec89cd906cd4ab7ca38ef4d59bba38f5feb2
Illan Gomez <illan91@hotmail.com>
2017-10-20 01:07:33 +0200

    [MAJ] libopt. ft_option_find now return a char** null terminated. It ensure to accepte any multiple value. Ex: './vm -n=1 -n=2 -n=3 -n=4'

--- libs/libopt/sources/ft_option_find.c
+++ libs/libopt/sources/ft_option_find.c
@@ -6,7 +6,7 @@
 /*   By: Zoellingam <illan91@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/11 23:11:12 by Zoellingam        #+#    #+#             */
-/*   Updated: 2017/10/02 02:12:17 by Zoellingam       ###   ########.fr       */
+/*   Updated: 2017/10/20 00:46:12 by Zoellingam       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,10 @@
 
 static int	ft_option_find_name(t_list *it, void const *name)
 {
-	t_option_list *node;
-
-	node = C_OPTION(it);
-	return (0 == ft_strcmp((char const *)name, node->name));
+	return (0 == ft_strcmp((char const *)name, C_OPTION(it)->name));
 }
 
-char		*ft_option_find(t_option *opt, char const *name)
+char		**ft_option_find(t_option *opt, char const *name)
 {
 	t_list	*it;
 
