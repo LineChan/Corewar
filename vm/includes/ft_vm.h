8ce03fd926f26bb2b7b63c48d3349d79957c3ab4
Mai Line VILLEMIN <mvillemi@student.42.fr>
2017-10-20 13:03:15 +0200

    Revert "update from original"

--- vm/includes/ft_vm.h
+++ vm/includes/ft_vm.h
@@ -3,30 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_vm.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Zoelling <Zoelling@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Zoellingam <illan91@hotmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/09/15 11:17:11 by Zoelling          #+#    #+#             */
-/*   Updated: 2017/10/20 00:05:13 by mvillemi         ###   ########.fr       */
+/*   Created: 2015/09/15 11:17:11 by Zoellingam        #+#    #+#             */
+/*   Updated: 2017/09/30 17:21:04 by Zoellingam       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_VM_H
 # define FT_VM_H
-
-/*
-** Personnal Libraries
-*/
-
-# include "op.h"
-# include "macro.h"
-# include "ft_option.h"
-# include "ft_printf.h"
-# include "ft_ctype.h"
-# include "ft_string.h"
-
-/*
-** Macros
-*/
 
 # ifdef DEBUG
 #  define DEBUG_MODE 1
@@ -40,22 +25,5 @@
 # ifndef EXIT_SUCCESS
 #  define EXIT_SUCCESS 0
 # endif
-
-/*
-** Structures
-*/
-
-typedef struct      s_vm
-{
-    char            arena[MEM_SIZE + 1];
-}                   t_vm;
-
-/*
-** Prototype
-*/
-
-int         ft_atoi(char *str);
-
-t_option    *ft_vm_option(int option[], int ac, char **av);
 
 #endif
