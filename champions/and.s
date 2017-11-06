a16c6f6f4965e5fbcb59c2c12d6910cf2a2b2f62
LineChan <mailine@gmail.com>
2017-11-06 02:03:37 +0100

    and corrected, do or and xor next time

--- champions/and.s
+++ champions/and.s
@@ -4,3 +4,4 @@
 and r0, r0, r1
 and r1, r1, r2
 and r1, r3, r1
+and 4080, r0, r3
--- vm/sources/instr_set/ft_vm_instr_and.c
+++ vm/sources/instr_set/ft_vm_instr_and.c
@@ -6,7 +6,7 @@
 /*   By: mvillemi <mvillemi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/05 23:29:24 by mvillemi          #+#    #+#             */
-/*   Updated: 2017/11/06 00:47:12 by mvillemi         ###   ########.fr       */
+/*   Updated: 2017/11/06 01:59:28 by mvillemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,23 +34,48 @@ void			ft_vm_instr_and(unsigned char arena[],
 	unsigned int	and[2];
 	unsigned char	*ptr;
 
-	(void)arena;
 	(void)dead_pool;
 	ptr = champ->pc + 2;
 	i = 0;
 	DEBUG_MODE ? ft_printf("{yellow:and}\n") : 0;
+	if (DEBUG_MODE)
+	{
+		ft_vm_print_reg(champ);
+		getchar();
+	}
 	while (i < (champ->instr.op->nb_args - 1))
 	{
 		if (champ->instr.op->arg_types[i] == T_REG)
+		{
+			ft_printf("T_REG : reg[%d]\n", (int)*ptr);
 			and[i] = champ->reg[*ptr];
+			ft_printf("and[%d] = %d\n", i, and[i]);
+			ft_vm_print_reg(champ);
+			getchar();
+		}
 		else if (champ->instr.op->arg_types[i] == T_IND)
-			and[i] = *(champ->pc + (ft_instruction_get_data(2, ptr) % IDX_MOD));
+		{
+			ft_printf("pc : %d\n", champ->pc - arena);
+			ft_printf("T_IND : %d\n", ft_instruction_get_data(2, ptr));
+			ft_printf("arena[%d] : %d\n", champ->pc - arena + ft_instruction_get_data(2, ptr), arena[champ->pc - arena + ft_instruction_get_data(2, ptr)]);
+			and[i] = arena[champ->pc - arena + ft_instruction_get_data(2, ptr)];
+			ft_printf("and[%d] = %d\n", i, and[i]);
+			ft_vm_print_reg(champ);
+			getchar();
+		}
 		else
-			and[i] = ft_instruction_get_data(g_direct_jump_table_from_instr[champ->instr.op->numero], ptr);
-		++i;
+		{
+			ft_printf("T_DIR\n");
+			and[i] = arena[ft_instruction_get_data(g_direct_jump_table_from_instr[champ->instr.op->numero], ptr)];
+			ft_printf("and[%d] = %d\n", i, and[i]);
+			ft_vm_print_reg(champ);
+			getchar();
+		}
 		ptr += champ->instr.arg_jump[i];
+		++i;
 	}
-	champ->reg[*ptr] = and[0] & and[1];
+	ft_printf("reg[%d] : and[0] : %d and[1] : %d ----> & %d \n", *ptr, and[0] , and[1], and[0] & and[1]);
+	champ->reg[ft_instruction_get_data(1, ptr)] = and[0] & and[1];
 	champ->pc += 2 + champ->instr.arg_jump[0] + champ->instr.arg_jump[1] + champ->instr.arg_jump[2];
 	if (DEBUG_MODE)
 	{
